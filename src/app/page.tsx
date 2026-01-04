710e79a27bae846f86e8761c5ff624ab41637588
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2026-01-04 07:13:29 +0700

    feat(ui): implement home page with FeedList and initial posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import FeedList from "@/components/feed/feed-list";
+import { getInitialPosts } from "@/lib/api/post";
+
 export default async function Home() {
+  const initialData = await getInitialPosts();
   return (
-    <div className="container mx-auto max-w-2xl py-6 px-4 md:px-0">Hello</div>
+    <div className="container mx-auto max-w-2xl py-6 px-4 md:px-0">
+      {" "}
+      <FeedList initialData={initialData} />
+    </div>
   );
 }
